1174669075165ef781b13c242c763b640d17092f
mabra <mabrabson@gmail.com>
2022-02-27 15:06:33 -0500

    Created Classes (PdfReport, Bill, Flatmate) but didn't properly write methods

---
+++ main.py
@@ -0,0 +1,44 @@
+class Bill:
+    """
+    Object that contains data about a bill
+    such as total amount and period of the bill.
+    """
+
+    def __init__(self, amount, period):
+        self.period = period
+        self.amount = amount
+
+
+class Flatmate:
+    """
+    Creates a flatmate person who lives in the
+    flat and pays a share of the bill.
+    """
+
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill):
+        return bill.amount/2
+
+
+class PdfReport():
+    """
+    Creates a pdf file that contains data about
+    the flatmates, their names, their amount owed,
+    and the period of the bill.
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        pass
+
+
+bill = Bill(amount=120, period="March 2021")
+john = Flatmate(name='John', days_in_house=20)
+marry = Flatmate(name='Marry', days_in_house=25)
+
+print(john.pays(bill=bill))
